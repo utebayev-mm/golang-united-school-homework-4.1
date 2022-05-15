fcd507c43ed5fc0816d45364f190be1b7e1e158c
utebayev.mm <utebayev.mm@gmail.com>
2022-05-15 18:42:54 +0600

    homework

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,8 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	for i := len(input) - 1; i >= 0; i-- {
+		output += string(input[i])
+	}
 	return output
 }
